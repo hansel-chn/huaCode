ee6b6269fd8d165dbfde7cfcf917d5008aadaeec
haozhenhua <haozhenhua@chiansec.com>
2024-03-11 10:07:06 +0800

    problem 354

--- leetcode/editor/cn/[354]Russian Doll Envelopes.go
+++ leetcode/editor/cn/[354]Russian Doll Envelopes.go
@@ -44,10 +44,15 @@ Output: 1
 package main
 
 import (
+	"math"
 	"sort"
 )
 
 func maxEnvelopes(envelopes [][]int) int {
+	if len(envelopes) == 0 {
+		return 0
+	}
+
 	sort.Slice(envelopes, func(i, j int) bool {
 		if envelopes[i][0] < envelopes[j][0] {
 			return true
@@ -62,12 +67,17 @@ func maxEnvelopes(envelopes [][]int) int {
 		dp[i] = 1
 	}
 
-	for i := 1; i < len(dp); i++ {
-		for j := 0; j < i; j++ {
-			if envelopes[j][1] < envelopes[i][1] {
-				dp[i] = max(dp[i], dp[j]+1)
-			}
+	assist := make([]int, 1)
+	assist[0] = math.MinInt
+
+	for i := 0; i < len(dp); i++ {
+		idx := sort.SearchInts(assist, envelopes[i][1])
+		if idx == len(assist) {
+			assist = append(assist, envelopes[i][1])
+		} else {
+			assist[idx] = envelopes[i][1]
 		}
+		dp[i] = idx
 	}
 
 	res := 0
@@ -76,5 +86,35 @@ func maxEnvelopes(envelopes [][]int) int {
 	}
 	return res
 }
+
+//func maxEnvelopes(envelopes [][]int) int {
+//	sort.Slice(envelopes, func(i, j int) bool {
+//		if envelopes[i][0] < envelopes[j][0] {
+//			return true
+//		} else if envelopes[i][0] == envelopes[j][0] && envelopes[i][1] > envelopes[j][1] {
+//			return true
+//		} else {
+//			return false
+//		}
+//	})
+//	dp := make([]int, len(envelopes))
+//	for i := range dp {
+//		dp[i] = 1
+//	}
+//
+//	for i := 1; i < len(dp); i++ {
+//		for j := 0; j < i; j++ {
+//			if envelopes[j][1] < envelopes[i][1] {
+//				dp[i] = max(dp[i], dp[j]+1)
+//			}
+//		}
+//	}
+//
+//	res := 0
+//	for _, count := range dp {
+//		res = max(res, count)
+//	}
+//	return res
+//}
 
 //leetcode submit region end(Prohibit modification and deletion)
