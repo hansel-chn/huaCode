182c2b59e54fa548f987b1be6cf24621eeba2a05
haozhenhua <haozhenhua@chiansec.com>
2024-06-11 09:43:32 +0800

    Problem 933

--- leetcode/editor/cn/[933]Number of Recent Calls.go
+++ leetcode/editor/cn/[933]Number of Recent Calls.go
@@ -1,24 +1,24 @@
 /**
-You have a RecentCounter class which counts the number of recent requests 
-within a certain time frame. 
+You have a RecentCounter class which counts the number of recent requests
+within a certain time frame.
 
- Implement the RecentCounter class: 
+ Implement the RecentCounter class:
 
- 
- RecentCounter() Initializes the counter with zero recent requests. 
- int ping(int t) Adds a new request at time t, where t represents some time in 
+
+ RecentCounter() Initializes the counter with zero recent requests.
+ int ping(int t) Adds a new request at time t, where t represents some time in
 milliseconds, and returns the number of requests that has happened in the past 30
-00 milliseconds (including the new request). Specifically, return the number of 
-requests that have happened in the inclusive range [t - 3000, t]. 
- 
+00 milliseconds (including the new request). Specifically, return the number of
+requests that have happened in the inclusive range [t - 3000, t].
 
- It is guaranteed that every call to ping uses a strictly larger value of t 
-than the previous call. 
 
- 
- Example 1: 
+ It is guaranteed that every call to ping uses a strictly larger value of t
+than the previous call.
 
- 
+
+ Example 1:
+
+
 Input
 ["RecentCounter", "ping", "ping", "ping", "ping"]
 [[], [1], [100], [3001], [3002]]
@@ -28,42 +28,45 @@ Output
 Explanation
 RecentCounter recentCounter = new RecentCounter();
 recentCounter.ping(1);     // requests = [1], range is [-2999,1], return 1
-recentCounter.ping(100);   // requests = [1, 100], range is [-2900,100], return 
+recentCounter.ping(100);   // requests = [1, 100], range is [-2900,100], return
 2
-recentCounter.ping(3001);  // requests = [1, 100, 3001], range is [1,3001], 
+recentCounter.ping(3001);  // requests = [1, 100, 3001], range is [1,3001],
 return 3
 recentCounter.ping(3002);  // requests = [1, 100, 3001, 3002], range is [2,3002]
 , return 3
- 
 
- 
- Constraints: 
 
- 
- 1 <= t <= 10⁹ 
- Each test case will call ping with strictly increasing values of t. 
- At most 10⁴ calls will be made to ping. 
- 
+
+ Constraints:
+
+
+ 1 <= t <= 10⁹
+ Each test case will call ping with strictly increasing values of t.
+ At most 10⁴ calls will be made to ping.
+
 
  Related Topics 设计 队列 数据流 👍 255 👎 0
 
 */
 
-//leetcode submit region begin(Prohibit modification and deletion)
+// leetcode submit region begin(Prohibit modification and deletion)
+package main
+
 type RecentCounter struct {
-
+	queue []int
 }
-
 
 func Constructor() RecentCounter {
-
+	return RecentCounter{queue: make([]int, 0)}
 }
-
 
 func (this *RecentCounter) Ping(t int) int {
-
+	this.queue = append(this.queue, t)
+	for this.queue[0] < t-3000 {
+		this.queue = this.queue[1:]
+	}
+	return len(this.queue)
 }
-
 
 /**
  * Your RecentCounter object will be instantiated and called as such:
