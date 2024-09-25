d9b4602f091232529537e0502b344983db58d8e4
haozhenhua <haozhenhua@chiansec.com>
2024-09-25 10:23:38 +0800

    Problem 746

---
+++ leetcode/editor/cn/[746]Min Cost Climbing Stairs.go
@@ -0,0 +1,59 @@
+//You are given an integer array cost where cost[i] is the cost of iᵗʰ step on
+//a staircase. Once you pay the cost, you can either climb one or two steps.
+//
+// You can either start from the step with index 0, or the step with index 1.
+//
+// Return the minimum cost to reach the top of the floor.
+//
+//
+// Example 1:
+//
+//
+//Input: cost = [10,15,20]
+//Output: 15
+//Explanation: You will start at index 1.
+//- Pay 15 and climb two steps to reach the top.
+//The total cost is 15.
+//
+//
+// Example 2:
+//
+//
+//Input: cost = [1,100,1,1,1,100,1,1,100,1]
+//Output: 6
+//Explanation: You will start at index 0.
+//- Pay 1 and climb two steps to reach index 2.
+//- Pay 1 and climb two steps to reach index 4.
+//- Pay 1 and climb two steps to reach index 6.
+//- Pay 1 and climb one step to reach index 7.
+//- Pay 1 and climb two steps to reach index 9.
+//- Pay 1 and climb one step to reach the top.
+//The total cost is 6.
+//
+//
+//
+// Constraints:
+//
+//
+// 2 <= cost.length <= 1000
+// 0 <= cost[i] <= 999
+//
+//
+// Related Topics 数组 动态规划 👍 1531 👎 0
+
+// leetcode submit region begin(Prohibit modification and deletion)
+package main
+
+func minCostClimbingStairs(cost []int) int {
+	dp := make([]int, len(cost))
+	dp[0] = cost[0]
+	dp[1] = cost[1]
+
+	for i := 2; i < len(cost); i++ {
+		dp[i] = min(dp[i-1], dp[i-2]) + cost[i]
+	}
+
+	return min(dp[len(cost)-1], dp[len(cost)-2])
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
