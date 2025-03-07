3a06a2bf934ef84287095d61bc388bebf920b81d
hansel <zhenhuahao.chn@gmail.com>
2025-03-07 15:06:49 +0800

    problem 971

---
+++ leetcode/editor/cn/[971]Flip Binary Tree To Match Preorder Traversal.go
@@ -0,0 +1,104 @@
+//You are given the root of a binary tree with n nodes, where each node is
+//uniquely assigned a value from 1 to n. You are also given a sequence of n values
+//voyage, which is the desired pre-order traversal of the binary tree.
+//
+// Any node in the binary tree can be flipped by swapping its left and right
+//subtrees. For example, flipping node 1 will have the following effect:
+//
+// Flip the smallest number of nodes so that the pre-order traversal of the
+//tree matches voyage.
+//
+// Return a list of the values of all flipped nodes. You may return the answer
+//in any order. If it is impossible to flip the nodes in the tree to make the pre-
+//order traversal match voyage, return the list [-1].
+//
+//
+// Example 1:
+//
+//
+//Input: root = [1,2], voyage = [2,1]
+//Output: [-1]
+//Explanation: It is impossible to flip the nodes such that the pre-order
+//traversal matches voyage.
+//
+//
+// Example 2:
+//
+//
+//Input: root = [1,2,3], voyage = [1,3,2]
+//Output: [1]
+//Explanation: Flipping node 1 swaps nodes 2 and 3, so the pre-order traversal
+//matches voyage.
+//
+// Example 3:
+//
+//
+//Input: root = [1,2,3], voyage = [1,2,3]
+//Output: []
+//Explanation: The tree's pre-order traversal already matches voyage, so no
+//nodes need to be flipped.
+//
+//
+//
+// Constraints:
+//
+//
+// The number of nodes in the tree is n.
+// n == voyage.length
+// 1 <= n <= 100
+// 1 <= Node.val, voyage[i] <= n
+// All the values in the tree are unique.
+// All the values in voyage are unique.
+//
+//
+// Related Topics 树 深度优先搜索 二叉树 👍 123 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+package main
+
+func flipMatchVoyage(root *TreeNode, voyage []int) []int {
+	var traverse func(root *TreeNode) bool
+	rlt := make([]int, 0)
+	var pos int
+
+	traverse = func(node *TreeNode) bool {
+		if node == nil {
+			return true
+		}
+
+		if node.Val != voyage[pos] {
+			return false
+		}
+		pos++
+
+		if node.Left != nil && node.Left.Val != voyage[pos] {
+			rlt = append(rlt, node.Val)
+			node.Left, node.Right = node.Right, node.Left
+		}
+
+		if !traverse(node.Left) {
+			return false
+		}
+
+		if !traverse(node.Right) {
+			return false
+		}
+		return true
+	}
+
+	if traverse(root) {
+		return rlt
+	} else {
+		return []int{-1}
+	}
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
